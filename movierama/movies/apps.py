a31733167362f4f6c409fea6abae7cdf0f83f8df
Petros Lamb <petroslamb@yahoo.gr>
2022-05-12 04:11:51 +0300

    fix: Remove unused code dependency

--- movierama/movies/apps.py
+++ movierama/movies/apps.py
@@ -1,11 +1,6 @@
 from django.apps import AppConfig
-from watson import search as watson
 
 
 class MoviesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "movierama.movies"
-
-    def ready(self):
-        Movie = self.get_model("Movie")
-        watson.register(Movie)
